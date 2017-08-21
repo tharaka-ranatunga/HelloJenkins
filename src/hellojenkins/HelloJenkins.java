140f6061112e007b9ae9a17c5c1d6840331bb9e9
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-08-21 09:08:37 +0530

    code changes

--- src/hellojenkins/HelloJenkins.java
+++ src/hellojenkins/HelloJenkins.java
@@ -17,14 +17,11 @@ public class HelloJenkins{
      */
     public static void main(String[] args) {
         int k;
-        for(int i=0; i<=10; i++){
-            System.out.println("Hello Jenkins" + i);
-        }
-        for(int i=0; i<=10; i++){
-            System.out.println("Hello Jenkins" + i);
-        }
-        sdfl;
-        // TODO code application logic here
+	for(int j=0; j<2; j++){
+		for(int i=0; i<=10; i++){
+           		System.out.println("Hello Jenkins" + i);
+        	}
+	}
     }
     
 }
